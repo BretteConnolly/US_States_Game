c5fb9ee30c61200ff0df07cf8b104424ecf7fa06
Brette Fitzgibbon <79230918+BretteConnolly@users.noreply.github.com>
2022-08-02 23:12:54 +0000

    Added comments

--- main.py
+++ main.py
@@ -5,30 +5,30 @@ screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
-turtle.shape(image)
+turtle.shape(image) #uses map as turtle obect
 
 data = pandas.read_csv("50_states.csv")
-all_states = data.state.to_list()
+all_states = data.state.to_list() #list of values from "state" column
 guessed_states = []
 score = 0
 
-while score < 50:
+while score < 50: #while not all the states have been guessed
   answer_state = screen.textinput(f"{score} / 50", "Name a U.S. state:").title()
   if answer_state == "Exit":
-    missing_states = []
+    missing_states = [] #states not guessed by the end
     for state in all_states:
       if state not in guessed_states:
         missing_states.append(state)
-    new_data = pandas.DataFrame(missing_states)
-    new_data.to_csv("states.to.learn.csv")
+    new_data = pandas.DataFrame(missing_states) #convert to DataFrame so this list can be stored in a CSV file as a Pandas series
+    new_data.to_csv("states.to.learn.csv") #file of missing states provided at the end to educate the user
     break
-  elif answer_state in all_states:
+  elif answer_state in all_states: #state must be one on the list of states
       placement = turtle.Turtle()
       placement.hideturtle()
       placement.penup()
-      state_row = data[data.state == answer_state]
-      placement.goto(int(state_row.x), int(state_row.y))
-      placement.write(answer_state)
+      state_row = data[data.state == answer_state] #row of the guessed state
+      placement.goto(int(state_row.x), int(state_row.y)) #coordinate values from this row
+      placement.write(answer_state) #labels state name onto map
       guessed_states.append(answer_state)
       score += 1
 
